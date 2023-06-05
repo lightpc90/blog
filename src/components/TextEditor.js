b4aad10e437d40617c3f435bdd5381e66b9d4ce7
lightpc90 <afoageektech@gmail.com>
2023-06-05 13:26:26 +0100

    refactor and added text editor component

---
+++ src/components/TextEditor.js
@@ -0,0 +1,38 @@
+import dynamic from 'next/dynamic';
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+import 'react-quill/dist/quill.snow.css';
+
+const TextEditor = ({ value, onChange }) => {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'list',
+    'bullet',
+    'link',
+    'image',
+  ];
+
+  return (
+    <ReactQuill
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      formats={formats}
+    />
+  );
+};
+
+export default TextEditor;
--- src/pages/Admin/NewPost.js
+++ src/pages/Admin/NewPost.js
@@ -1,34 +1,42 @@
 import React, {useState} from 'react'
 import {Grid, Input, Text, Button, Row, Textarea, Card, Col} from '@nextui-org/react'
 import AddPost from '@/firebase/AddPost'
+import TextEditor from '@/components/TextEditor'
 
 
 const NewPost = () => {
 
     const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
     const [content, setContent] = useState('')
 
-    const handleSubmit = async() => { 
-        const Post = {title: title, content: content}
+    const handleEditorChange = (value) => {
+        setContent(value) 
+    }
+    const handleSubmit = async() => {
+        const Post = {title: title, description: description, content: content}
         const {result, error} = await AddPost(Post)
+
     }
 
 
   return (
     <Grid.Container gap={4} css={{ paddingLeft:50, paddingRight:50 }} justify='center' >
-        <Grid md={12}> <Text weight='bold'>Admin Page to Add A New Post</Text></Grid>
+        <Grid md={12}> <Text size={20} weight='bold'>Admin Page: Add A New Post</Text></Grid>
          <Card css={{width:3000, }}>
             <Card.Header>
                 <Card.Divider>
-                <Text color='secondary'>Add A New Post</Text>
+                <Text color='secondary' weight='bold'>Add A New Post</Text>
                 </Card.Divider> 
             </Card.Header>
             
             <Card.Body>    
                 <Grid.Container gap={2} >
                     <Grid xs={12} sm={12} md={12}>
+                        <Col>
+                        <Text color='secondary' weight='bold'>Post Title</Text>
                         <Input
-                            css={{ width: 1000}}
+                            css={{ width: '60%'}}
                             bordered
                             placeholder='Your Post Title Here!'
                             color='secondary'
@@ -37,20 +45,32 @@ const NewPost = () => {
                             onChange={(e) => setTitle(e.target.value)}
                             value={title}
                         />
+                        </Col>
                     </Grid>
 
                     <Grid md={12} xs={12} sm={12}>
-                        <Row>
+                        <Col>
+                        <Text color='secondary' weight='bold'>Post Desciption</Text>
                         <Textarea
-                            css={{width: 1000}}
-                            placeholder='Your Post content here...'
+                            css={{width: '60%'}}
+                            placeholder='Your Post desciption here!'
                             bordered
-                            aria-label='content'
+                            aria-label='description'
                             color='secondary'
-                            onChange={(e) => setContent(e.target.value)}
-                            value={content}
+                            onChange={(e) => setDescription(e.target.value)}
+                            value={description}
                         />
-                        </Row>  
+                        </Col>  
+                    </Grid>
+                    <Grid md={12} xs={12} sm={12}>
+                        <Col>
+                        <Text color='secondary' weight='bold'>Post Contents</Text>
+                        <TextEditor
+                            css={{width: '60%'}}
+                            value={content}
+                            onChange={handleEditorChange}
+                        />
+                        </Col>  
                     </Grid>
                     </Grid.Container>    
                 <Grid md={12} xs={12} sm={12}>
@@ -58,7 +78,7 @@ const NewPost = () => {
                     color='secondary'  
                     type="submit" auto 
                     onPress={handleSubmit}>
-                     Add Post
+                     Add New Post
                  </Button>
                 </Grid> 
             </Card.Body>
